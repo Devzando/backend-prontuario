47aa9413cdcbaa2bf085a1531dee90dbc4e7b4b7
cLausx0 <david.almeida2707@gmail.com>
2024-10-24 01:13:09 -0300

    feat: criando usecase de retornar paciente por filtro

---
+++ prontuario.Application/Usecases/Patient/GetPatientsByFilterUseCase.cs
@@ -0,0 +1,29 @@
+﻿using prontuario.Application.Gateways;
+using prontuario.Domain.Entities;
+using prontuario.Domain.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prontuario.Application.Usecases.Patient
+{
+    public class GetPatientsByFilterUseCase
+    {
+        private readonly IGatewayPatient _gatewayPatient;
+
+        public GetPatientsByFilterUseCase(IGatewayPatient gatewayPatient)
+        {
+            _gatewayPatient = gatewayPatient;
+        }
+
+        public async Task<ResultPattern<PatientEntity>> Execute(string filter)
+        {
+            var result = await _gatewayPatient.GetByFilter(filter);
+            return result != null 
+                ? ResultPattern<PatientEntity>.SuccessResult(result) 
+                : ResultPattern<PatientEntity>.FailureResult("Paciente não encontrado!", 404);
+        }
+    }
+}
