1e22c6edb672f08d78cf7139a8a416a827dbe81d
cLausx0 <david.almeida2707@gmail.com>
2024-10-24 01:14:05 -0300

    feat: adicionando usecase de buscar paciente por filtro nas dependencias

--- prontuario.WebApi/Config/IocDependencyExtensions.cs
+++ prontuario.WebApi/Config/IocDependencyExtensions.cs
@@ -12,6 +12,7 @@ namespace prontuario.WebApi.Config
             services.AddScoped<IGatewayPatient, PatientRepositoryGateway>();
             services.AddScoped<CreatePatientUseCase>();
             services.AddScoped<GetAllPatientsUseCase>();
+            services.AddScoped<GetPatientsByFilterUseCase>();
         }
     }
 }
