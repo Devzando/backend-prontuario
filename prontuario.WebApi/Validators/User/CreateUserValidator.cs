86aa81b80b8ff2d9d9cfc4f0f924014623d5042c
cLausx0 <david.almeida2707@gmail.com>
2025-01-29 19:23:32 -0300

    refactor: retirando valor HEALTHTEAM e adicionando DOCTOR e NURSE na validacao do campo de Role do usuario

--- prontuario.WebApi/Validators/User/CreateUserValidator.cs
+++ prontuario.WebApi/Validators/User/CreateUserValidator.cs
@@ -30,8 +30,8 @@ namespace prontuario.WebApi.Validators.User
             RuleFor(x => x.Profile.Role)
                 .NotNull().WithMessage("O campo Role não pode ser nulo.")
                 .NotEmpty().WithMessage("O campo Role é obrigatório.")
-                .Must(role => role == "ADMIN" || role == "RECEPTIONTEAM" || role == "HEALTHTEAM" || role == "INTITUATIONMANAGEMENT")
-                .WithMessage("O campo Role deve ser ADMIN, RECEPTIONTEAM, HEALTHTEAM ou INTITUATIONMANAGEMENT");
+                .Must(role => role == "ADMIN" || role == "RECEPTIONTEAM" || role == "DOCTOR" || role == "NURSE" || role == "INTITUATIONMANAGEMENT")
+                .WithMessage("O campo Role deve ser ADMIN, RECEPTIONTEAM, DOCTOR, NURSE ou INTITUATIONMANAGEMENT");
         }
     }
 }
