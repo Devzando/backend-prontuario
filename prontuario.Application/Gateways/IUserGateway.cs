5886249429be7b767790c425c595f10043f3e8b3
cLausx0 <david.almeida2707@gmail.com>
2025-01-29 17:51:46 -0300

    feat: adicionando metodo de retornar usuario pelo Id

--- prontuario.Application/Gateways/IUserGateway.cs
+++ prontuario.Application/Gateways/IUserGateway.cs
@@ -5,6 +5,7 @@ namespace prontuario.Application.Gateways;
 public interface IUserGateway
 {
     Task<UserEntity?> FindUserByEmail(string userEmail);
+    Task<UserEntity?> FindUserById(long userId);
     Task Create(UserEntity userEntity);
     Task<UserEntity> Update(UserEntity userEntity);
 }
